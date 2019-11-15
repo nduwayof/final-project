b3b93c217b4de3b8d9bf095bbdec6e56c361efa6
Nduwayo Fabrice <nduwayof@outlook.com>
2019-11-14 20:53:15 -0600

    Created the restaurant service project

---
+++ restaurants-service/src/main/java/edu/mum/cs/restaurants/RestaurantsServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.restaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantsServiceApplication.class, args);
+    }
+
+}
