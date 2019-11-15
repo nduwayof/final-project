f05b85a730a3e51ea93fff09d6db09f90e3036d4
Nduwayo Fabrice <nduwayof@outlook.com>
2019-11-14 21:05:17 -0600

    Added the server port into the application.properties

--- restaurants-service/src/test/java/edu/mum/cs/restaurants/RestaurantsServiceApplicationTests.java
+++ restaurants-service/src/test/java/edu/mum/cs/restaurants/RestaurantsServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package edu.mum.cs.restaurants;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestaurantsServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
