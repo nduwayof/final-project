2f83b0472236c30c87e55a6c61856fc11d957333
C. Hoffmann <caiohoffmann@gmail.com>
2019-11-15 18:11:48 -0600

    Minor refatoring

--- Favorites/app.js
+++ Favorites/app.js
@@ -3,22 +3,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const { Kafka } = require('kafkajs');
+
 const mongoose = require('mongoose');
 
 const favoritesRouter = require('./routes/favorites');
-const Favorites = require('./models/favorites');
+const listener = require('./listener/listener');
 
 const app = express();
 
-const kafka = new Kafka({
-  clientId: 'favorites',
-  brokers: ['my-kafka:9092']
-});
-
-
-const producer = kafka.producer();
-const consumer = kafka.consumer({ groupId: 'ea-eats' });
 
 
 app.use(logger('dev'));
@@ -39,42 +31,9 @@ mongoose.connect('mongodb://mongodb:27017/favorites', err => {
   }
 });
 
-// Consuming
-await consumer.connect()
-await consumer.subscribe({ topic: 'user' });
-await consumer.subscribe({ topic: 'rank' });
-
-await consumer.run({
-  eachMessage: async ({ topic, partition, message }) => {
-
-    if (topic === 'user') {
-      const user = JSON.parse(message.value);
-      try {
-        const fav = await Favorites.findOne({ user: user.id });
-        if (fav === null) {
-          const newFav = new Favorites({
-            user: user.id
-          });
-          const f = await newFav.save();
-        }
-      } catch (err) {
-        throw err;
-      }
-
-    } else if (topic === 'rank') {
-      const rest = JSON.parse(message.value);
-      const rank = rest.votes[rest.votes.length - 1];
-      const fav = await Favorites.findOne({ user: rank.user, 'restaurants.ref': rest.restaurant });
-      if (fav === null) {
-        const newFav = await Favorites.findOneAndUpdate({ user: rank.user }, { $push: { restaurants: { ref: rest.restaurant, rating: rank.stars } } });
-      } else {
-        const upFav = await Favorites.findOneAndUpdate({ user: rank.user, 'restaurants.ref': rest.restaurant }, { $set: { 'restaurants.$.rating': rank.stars, 'restaurants.$.ref': rest.restaurant } }, { new: true, upsert: true }).exec();
-      }
-    }
-  }
-});
-
 app.use('/favorites', favoritesRouter);
+
+listener().catch(console.error);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ Favorites/listener/listener.js
@@ -0,0 +1,49 @@
+const { Kafka } = require('kafkajs');
+
+const Favorites = require('../models/favorites');
+
+const kafka = new Kafka({
+    clientId: 'favorites',
+    brokers: ['my-kafka:9092']
+});
+
+const run = async _ => {
+    const producer = kafka.producer();
+    const consumer = kafka.consumer({ groupId: 'ea-eats' });
+
+    // Consuming
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'user' });
+    await consumer.subscribe({ topic: 'rank' });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+
+            if (topic === 'user') {
+                const user = JSON.parse(message.value);
+                try {
+                    const fav = await Favorites.findOne({ user: user.id });
+                    if (fav === null) {
+                        const newFav = new Favorites({
+                            user: user.id
+                        });
+                        const f = await newFav.save();
+                    }
+                } catch (err) {
+                    throw err;
+                }
+
+            } else if (topic === 'rank') {
+                const rest = JSON.parse(message.value);
+                const rank = rest.votes[rest.votes.length - 1];
+                const fav = await Favorites.findOne({ user: rank.user, 'restaurants.ref': rest.restaurant });
+                if (fav === null) {
+                    const newFav = await Favorites.findOneAndUpdate({ user: rank.user }, { $push: { restaurants: { ref: rest.restaurant, rating: rank.stars } } });
+                } else {
+                    const upFav = await Favorites.findOneAndUpdate({ user: rank.user, 'restaurants.ref': rest.restaurant }, { $set: { 'restaurants.$.rating': rank.stars, 'restaurants.$.ref': rest.restaurant } }, { new: true, upsert: true }).exec();
+                }
+            }
+        }
+    });
+}
+module.exports = run;
