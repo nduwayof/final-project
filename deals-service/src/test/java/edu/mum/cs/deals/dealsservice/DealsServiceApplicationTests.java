9149a1bc913249b82a6b95816b58d235625d7e4d
Nduwayo Fabrice <nduwayof@outlook.com>
2019-11-16 16:15:11 -0600

    Started to work on a new service
    Started to work on the deals service

--- deals-service/src/test/java/edu/mum/cs/deals/dealsservice/DealsServiceApplicationTests.java
+++ deals-service/src/test/java/edu/mum/cs/deals/dealsservice/DealsServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DealsServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
