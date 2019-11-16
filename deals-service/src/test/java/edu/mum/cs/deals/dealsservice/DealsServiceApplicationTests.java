9b5f58d030bba25b755330e8da1da9701a225422
Nduwayo Fabrice <nduwayof@outlook.com>
2019-11-16 16:13:36 -0600

    created a new service
    Created the deals service project

---
+++ deals-service/src/test/java/edu/mum/cs/deals/dealsservice/DealsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.deals.dealsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
