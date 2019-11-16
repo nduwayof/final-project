7823b32388716b409f6e6b013b4d719bdcb3cc1d
C. Hoffmann <caiohoffmann@gmail.com>
2019-11-15 18:02:02 -0600

    Fix in lisining to user

--- Favorites/app.js
+++ Favorites/app.js
@@ -39,42 +39,40 @@ mongoose.connect('mongodb://mongodb:27017/favorites', err => {
   }
 });
 
-const run = async () => {
+// Consuming
+await consumer.connect()
+await consumer.subscribe({ topic: 'user' });
+await consumer.subscribe({ topic: 'rank' });
 
-  // Consuming
-  await consumer.connect()
-  await consumer.subscribe({ topic: 'user' });
-  await consumer.subscribe({ topic: 'rank' });
+await consumer.run({
+  eachMessage: async ({ topic, partition, message }) => {
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-
-      if (topic === 'user') {
-        const user = JSON.parse(message.value);
-        try {
+    if (topic === 'user') {
+      const user = JSON.parse(message.value);
+      try {
+        const fav = await Favorites.findOne({ user: user.id });
+        if (fav === null) {
           const newFav = new Favorites({
             user: user.id
           });
           const f = await newFav.save();
-        } catch (err) {
-          throw err;
         }
+      } catch (err) {
+        throw err;
+      }
 
-      } else if (topic === 'rank') {
-        const rest = JSON.parse(message.value);
-        const rank = rest.votes[rest.votes.length - 1];
-        const fav = await Favorites.findOne({ user: rank.user, 'restaurants.ref': rest.restaurant });
-        if (fav === null) {
-          const newFav = await Favorites.findOneAndUpdate({ user: rank.user }, { $push: { restaurants: { ref: rest.restaurant, rating: rank.stars } } });
-        } else {
-          const upFav = await Favorites.findOneAndUpdate({ user: rank.user, 'restaurants.ref': rest.restaurant }, { $set: { 'restaurants.$.rating': rank.stars, 'restaurants.$.ref': rest.restaurant } }, { new: true, upsert: true }).exec();
-        }
+    } else if (topic === 'rank') {
+      const rest = JSON.parse(message.value);
+      const rank = rest.votes[rest.votes.length - 1];
+      const fav = await Favorites.findOne({ user: rank.user, 'restaurants.ref': rest.restaurant });
+      if (fav === null) {
+        const newFav = await Favorites.findOneAndUpdate({ user: rank.user }, { $push: { restaurants: { ref: rest.restaurant, rating: rank.stars } } });
+      } else {
+        const upFav = await Favorites.findOneAndUpdate({ user: rank.user, 'restaurants.ref': rest.restaurant }, { $set: { 'restaurants.$.rating': rank.stars, 'restaurants.$.ref': rest.restaurant } }, { new: true, upsert: true }).exec();
       }
     }
-  });
-}
-
-run().catch(console.error);
+  }
+});
 
 app.use('/favorites', favoritesRouter);
 
