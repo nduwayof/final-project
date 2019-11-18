8b02548ef310f1def1d3ecaeb65d86dba71a1166
Nduwayo Fabrice <nduwayof@outlook.com>
2019-11-18 17:25:49 -0600

    Added jUnit Test
    Added a new test method with mockito

--- deals-service/src/main/java/edu/mum/cs/deals/DealsServiceApplication.java
+++ deals-service/src/main/java/edu/mum/cs/deals/DealsServiceApplication.java
@@ -2,6 +2,7 @@ package edu.mum.cs.deals;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author nduwayofabrice
  * @version 1.0
  */
+@EnableCaching
 @SpringBootApplication
 @EnableFeignClients("edu.mum.cs.deals")
 public class DealsServiceApplication {
--- restaurants-service/src/test/java/edu/mum/cs/restaurants/RestaurantsServiceApplicationTests.java
+++ restaurants-service/src/test/java/edu/mum/cs/restaurants/RestaurantsServiceApplicationTests.java
@@ -1,8 +1,66 @@
 package edu.mum.cs.restaurants;
 
+import edu.mum.cs.restaurants.models.Restaurant;
+import edu.mum.cs.restaurants.models.RestaurantAddress;
+import edu.mum.cs.restaurants.models.RestaurantMenu;
+import edu.mum.cs.restaurants.models.RestaurantSchedule;
+import edu.mum.cs.restaurants.repositories.IRestaurantRepository;
+import edu.mum.cs.restaurants.services.IRestaurantQueryService;
+import edu.mum.cs.restaurants.services.IRestaurantService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+/**
+ * The type Restaurants service application tests.
+ * @author nduwayofabrice
+ * @version 1.0
+ */
 @SpringBootTest
-class RestaurantsServiceApplicationTests {
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class RestaurantsServiceApplicationTests {
+
+    @MockBean
+    private IRestaurantRepository restaurantRepository;
+
+    @Autowired
+    private IRestaurantService restaurantService;
+
+    @Autowired
+    private IRestaurantQueryService restaurantQueryService;
+
+
+    @Test
+    public void getRestaurantsTest(){
+        List<RestaurantAddress> restaurantAddresses = new ArrayList<>();
+        restaurantAddresses.add(new RestaurantAddress());
+
+        List<RestaurantMenu> restaurantMenus = new ArrayList<>();
+        restaurantMenus.add(new RestaurantMenu());
+
+        List<RestaurantSchedule> restaurantSchedules = new ArrayList<>();
+        restaurantSchedules.add(new RestaurantSchedule());
+
+        when(restaurantRepository.findAll()).thenReturn(Stream.of(
+                new Restaurant("Every Body", "3124688754",restaurantAddresses,restaurantMenus, restaurantSchedules))
+                .collect(Collectors.toList()));
+        assertEquals(1, restaurantQueryService.findAllRestaurants().size());
+    }
 
 }
