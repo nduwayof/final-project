e20bc47b3a4c3202d75c7183ec165d3ef5dde353
C. Hoffmann <caiohoffmann@gmail.com>
2019-11-18 17:32:51 -0600

    Minor fix kafka

--- Favorites/listener/listener.js
+++ Favorites/listener/listener.js
@@ -34,11 +34,11 @@ const run = async _ => {
 
             } else if (topic === 'rank') {
                 const rest = JSON.parse(message.value);
-                const fav = await Favorites.findOne({ user: rank.userId, 'restaurants.ref': rest.rest_id });
+                const fav = await Favorites.findOne({ user: rank.userid, 'restaurants.ref': rest.rest_id });
                 if (fav === null) {
-                    const newFav = await Favorites.findOneAndUpdate({ user: rest.userId }, { $push: { restaurants: { ref: rest.rest_id, rating: rest.vote } } });
+                    const newFav = await Favorites.findOneAndUpdate({ user: rest.userid }, { $push: { restaurants: { ref: rest.rest_id, rating: rest.vote } } });
                 } else {
-                    const upFav = await Favorites.findOneAndUpdate({ user: rest.userId, 'restaurants.ref': rest.rest_id }, { $set: { 'restaurants.$.rating': rest.vote, 'restaurants.$.ref': rest.restaurant } }, { new: true, upsert: true }).exec();
+                    const upFav = await Favorites.findOneAndUpdate({ user: rest.userid, 'restaurants.ref': rest.rest_id }, { $set: { 'restaurants.$.rating': rest.vote, 'restaurants.$.ref': rest.restaurant } }, { new: true, upsert: true }).exec();
                 }
             }
         }
