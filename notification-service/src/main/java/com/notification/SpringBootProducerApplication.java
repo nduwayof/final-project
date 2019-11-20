02dd70a45e308ed50ae0b06c126aa0cc712e42e7
Afify <a.heshamshaaban@gmail.com>
2019-11-19 23:00:17 -0600

    adding logs and calling to user api

--- notification-service/src/main/java/com/notification/SpringBootProducerApplication.java
+++ notification-service/src/main/java/com/notification/SpringBootProducerApplication.java
@@ -17,29 +17,5 @@ public class SpringBootProducerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProducerApplication.class, args);
 	}
-	
-	@Value("${EMAIL}")
-	private String sender;
-	
-	@Value("${PASSWORD}")
-	private String password;
-	
-	@Bean
-	public JavaMailSender getJavaMailSender() {
-	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	    mailSender.setHost("smtp.gmail.com");
-	    mailSender.setPort(587);
-	     
-	    mailSender.setUsername(sender);
-	    mailSender.setPassword(password);
-	     
-	    Properties props = mailSender.getJavaMailProperties();
-	    props.put("mail.transport.protocol", "smtp");
-	    props.put("mail.smtp.auth", "true");
-	    props.put("mail.smtp.starttls.enable", "true");
-	    props.put("mail.debug", "true");
-	     
-	    return mailSender;
-	}
 
 }
